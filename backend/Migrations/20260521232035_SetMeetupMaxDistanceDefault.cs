3998a938c9d73c0fcebc015493469afc8d4cb93b
Chenghong Li <lch741@foxmail.com>
2026-05-22 11:22:36 +1200

    Finished data migration and added MaxDistanceKm property to MeetupEvent and set default value to 20

---
+++ backend/Migrations/20260521232035_SetMeetupMaxDistanceDefault.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMeetupMaxDistanceDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxDistanceKm",
+                table: "MeetupEvents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 20,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 0);
+
+            migrationBuilder.Sql(
+                "UPDATE \"MeetupEvents\" SET \"MaxDistanceKm\" = 20 WHERE \"MaxDistanceKm\" = 0;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MaxDistanceKm",
+                table: "MeetupEvents",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 20);
+        }
+    }
+}
